198e4305e805d62ee02e1170b4e87759cee8c4ba
Subrata Dey <deysubrata698@gmail.com>
2025-04-30 08:00:26 +0530

    first commit

---
+++ Lecture_1_intro/index.js
@@ -0,0 +1,7 @@
+console.log("Welcome Node Js Worldnode")
+
+console.log(5*10);
+
+console.log('aj 30/04/2025');
+console.log('nodemon install Successfully...!')
+console.log('nodemon install Successfully...2!')
---
+++ Lecture_2_Modules/index.js
@@ -0,0 +1,10 @@
+// console.log('We are Learning about the modules in node.js')
+
+// const {sum,sub,mul,div} = require('./utils');
+
+import {sum,sub,mul,div} from './utils.js';
+
+console.log("adding two number= ", sum(10, 5));
+console.log("adding two number= ", sub(10, 5));
+console.log("adding two number= ", mul(10, 5));
+console.log("adding two number= ", div(10, 5));
